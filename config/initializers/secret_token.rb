dbba081e6f9cb65eb36902f03441513751895f13
jChaseFreeman <j.chase.f@gmail.com>
2014-03-12 17:17:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DigitalOcean::Application.config.secret_key_base = 'fe6770e5bc08a57cdfce7db105e06e1c14b506a83ff3bd73c608e81eace43622c6cbb9b3efc3f6f4a11132bd217060ab269f8ee4fbd9c7b5bd29549db6abcef0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DigitalOcean::Application.config.session_store :cookie_store, key: '_digital-ocean_session'
