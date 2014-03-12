1db19eef9604bb65337db33f83b91cd8989c8a63
jChaseFreeman <j.chase.f@gmail.com>
2014-03-12 18:21:11 -0500

    prelaunch config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@107.170.54.97}
+role :web, %w{deploy@107.170.54.97}
+role :db,  %w{deploy@107.170.54.97}
 
 # Extended Server Syntax
 # ======================
